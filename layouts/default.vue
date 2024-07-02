de5833ba62167117d3e6e5432c1029b26397707a
maxvyr <60290207+Maxvyr@users.noreply.github.com>
2024-07-02 13:22:56 +0200

    tuto nuxt js doc

---
+++ layouts/default.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <h1 class="text-4xl font-bold">
+      Some default layout content shared across all pages
+    </h1>
+    <slot />
+  </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <h1>Welcome to the homepage</h1>
+  <AppAlert> This is an auto-imported component </AppAlert>
+  <div class="flex flex-row items-center justify-between p-4">
+    <nav>
+      <ul>
+        <li><NuxtLink to="/about">About</NuxtLink></li>
+        <li><NuxtLink to="/posts/1">Post 1</NuxtLink></li>
+        <li><NuxtLink to="/posts/2">Post 2</NuxtLink></li>
+        <li><NuxtLink to="/dashboard">Dashboard</NuxtLink></li>
+        <li><NuxtLink to="/login">Login</NuxtLink></li>
+      </ul>
+    </nav>
+    <img class="size-1/2" src="/img/nuxt.png" alt="Discover Nuxt 3" />
+  </div>
+</template>
